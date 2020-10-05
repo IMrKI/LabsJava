39b52fc9b916a269b79b0903f5befb64474a0752
Yan <yan.volovik@gmail.com>
2020-10-05 11:58:47 +0300

    Lab #1

---
+++ src/main.java
@@ -0,0 +1,27 @@
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Witaj w świecie Javy!");
+
+        System.out.println("Ile lat uczysz się programowania: ");
+        System.out.println("1,5");
+
+        System.out.println("Ile języków (programowania) znasz na poziomie co najmniej podstawowym: ");
+        System.out.println("Java\nPython\nC++");
+
+        System.out.println("Ulubiony język programowania poznany do tej pory: ");
+        System.out.println("Java");
+
+
+        int x, y;
+        int z = 12;
+        double i = 2.45;
+        char znak = 'z';
+        double j;
+        x = 0;
+        y = 444;
+        z = 13;
+        j = 0.002;
+        System.out.print("x = " + x + ", y = " + y + ", z = " + z + ", j = " + j + " Znak = " + znak);
+    }
+}
